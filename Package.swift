84eadbde55da203db486161fad5dd382ee718273
ZeroC <git@zeroc.com>
2026-03-07 03:42:53 +0000

    ice: 3.9.0-nightly.20260307.1 nightly build

--- Package.swift
+++ Package.swift
@@ -67,24 +67,24 @@ let package = Package(
         ),
         .binaryTarget(
             name: "IceCpp",
-            url: "https://download.zeroc.com/ice/nightly/3.9/Ice-3.9.0-nightly.20260306.3.xcframework.zip",
-            checksum: "df644aac80a405e14e7b1aa67400169ec95896a89128db556a8738e009687d19"
+            url: "https://download.zeroc.com/ice/nightly/3.9/Ice-3.9.0-nightly.20260307.1.xcframework.zip",
+            checksum: "11339e18a82ccb3f5f3aa0afa55790c81397bb43e4eff48d260004ff949ef53f"
         ),
         .binaryTarget(
             name: "IceDiscoveryCpp",
-            url: "https://download.zeroc.com/ice/nightly/3.9/IceDiscovery-3.9.0-nightly.20260306.3.xcframework.zip",
-            checksum: "887bdfd7b583c4263696dcf24ef550579270c3b3abf4c66e3f32eeea460a3988"
+            url: "https://download.zeroc.com/ice/nightly/3.9/IceDiscovery-3.9.0-nightly.20260307.1.xcframework.zip",
+            checksum: "4973467800b4a544d47e363d7e1bef3daa8a553cb01bf81b8e1d471ca495c1d7"
 
         ),
         .binaryTarget(
             name: "IceLocatorDiscoveryCpp",
-            url: "https://download.zeroc.com/ice/nightly/3.9/IceLocatorDiscovery-3.9.0-nightly.20260306.3.xcframework.zip",
-            checksum: "0907b6a800698a3a17d2e305881aa8e546a0497912ef0c16da19f91aea175080"
+            url: "https://download.zeroc.com/ice/nightly/3.9/IceLocatorDiscovery-3.9.0-nightly.20260307.1.xcframework.zip",
+            checksum: "5e7dc84cb45af5525cd948c85f92dea444ecdcc13d33f491ae8ec74173b2e427"
         ),
         .binaryTarget(
             name: "slice2swift",
-            url: "https://download.zeroc.com/ice/nightly/3.9/slice2swift-3.9.0-nightly.20260306.3.artifactbundle.zip",
-            checksum: "7d2656c4da17325748a7fef05cff87434972c412d58eb29573cd874ccc2508bb"
+            url: "https://download.zeroc.com/ice/nightly/3.9/slice2swift-3.9.0-nightly.20260307.1.artifactbundle.zip",
+            checksum: "95fe74d529eccd049dae8ede99311e5daef362cd97cc74361e60b61b0449267d"
         ),
         .plugin(
             name: "CompileSlice",
